b54d16cb7a30a1bf282c9f4d0e625dafd0e068bc
Nils Lundquist <nils@bitovi.com>
2017-11-08 11:52:44 -0700

    add NPM versioning / release scripts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import KeyMask from './key-mask';
 import callbacks from 'can-view-callbacks';
 
 // TODO: write readme
+// TODO: add pre-version NPM script
 
 callbacks.attr('key-mask', function(el) {
   const keymask = new KeyMask(el);
